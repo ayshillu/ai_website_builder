e6ee5bba3e29ddf09936708d77b22f339d8357d6
Aysha <aysha@example.com>
2025-04-12 00:07:03 +0530

    Updated build.sh with explicit Django installation and migrations

--- build.sh
+++ build.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+# upgrade pip
 python -m pip install --upgrade pip
+
+# install requirements
 pip install -r requirements.txt
+
+# install django explicitly
+pip install django
+
+# run django commands
+python manage.py makemigrations
+python manage.py migrate
 python manage.py collectstatic --noinput
